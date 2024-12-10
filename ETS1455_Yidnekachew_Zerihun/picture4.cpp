959f76773f8c7d56155b837e6a09dd33ebd0023f
ofx-yd <yidnekachew.zerihun@aastustudent.edu.et>
2024-12-10 19:50:55 +0300

    Create picture4.cpp

---
+++ ETS1455_Yidnekachew_Zerihun/picture4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   
+  for(int i=1;i<=5;i++){
+      char a='A';
+      for(int k=5-i;k>0;k--)
+      cout<<" ";
+    for(int j=i;j>=1;j--){
+      cout<<j;
+     
+      }
+      
+  cout<<endl;
+  
+  }
+  return 0;
+}
