b418afc717c9bacef7d4c291d52ff2c5bf9f9f7f
ofx-yd <yidnekachew.zerihun@aastustudent.edu.et>
2024-12-11 15:21:19 +0300

    Update and rename picture2.cpp to updated_picture2.cpp

---
+++ ETS1455_Yidnekachew_Zerihun/updated_picture2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int menu;
+    while(true){
+        
+        cout<<"***** MENU*******"<<endl;
+        cout<<"Press 1 to See example.\n";
+        cout<<"Press 2 to do with ur dimension.\n";
+        cout<<"Press 0 to terminate. \n";
+        cin>>menu;
+
+  switch (menu){
+     case 1:
+        for(int i=1;i<=5;i++){
+          char a='A';
+          for(int k=5-i;k>0;k--)
+          cout<<" ";
+            for(int j=i;j>=1;j--){
+            cout<<j;
+      }cout<<endl;
+  }
+     break;
+        
+     case 2:
+        int num;
+        cout<<"enter number";
+        cin>>num;
+   
+       for(int i=1;i<=num;i++){
+          char a='A';
+          for(int k=num-i;k>0;k--)
+          cout<<" ";
+            for(int j=i;j>=1;j--){
+            cout<<j;
+      }cout<<endl;
+  }
+     break;
+
+    case 0:
+        cout<<"\nThe program is terminated! Thank you..."<<endl;
+        return 0;
+
+    }
+    }
+    
+
+    EXIT_SUCCESS;
+}
