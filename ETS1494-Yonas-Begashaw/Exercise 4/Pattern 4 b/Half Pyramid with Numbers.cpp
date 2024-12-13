1c9ca721556552846b0c303554b3325ada2c0191
Yonas Begashaw <yonas.begashaw@aastustudent.edu.et>
2024-12-13 09:05:05 +0300

    Update Half Pyramid with Numbers.cpp

--- ETS1494-Yonas-Begashaw/Exercise 4/Pattern 4 b/Half Pyramid with Numbers.cpp
+++ ETS1494-Yonas-Begashaw/Exercise 4/Pattern 4 b/Half Pyramid with Numbers.cpp
@@ -2,21 +2,17 @@
 using namespace std;
 int main () {
 
-int row, column;
+int row;
 
 cout << "Enter the number of rows" << endl;
 cin >> row;
 
-cout << "Enter the number of columns" << endl;
-cin >> column;
-char letter = 'A';
 
 for (int i=1; i<= row; i++){
 
-    for (int j=1; j <= column; j++){
+    for (int j=1; j <= i; j++){
 
-        cout << letter;
-        letter += 1;
+        cout << j ;
     }
 
     cout << endl;
