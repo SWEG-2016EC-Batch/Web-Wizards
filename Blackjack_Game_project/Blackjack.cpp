112dc5edcd026609ea3a7db0e1fb85b6affb6c42
yidenek5 <yeabsira.ayele@aastustudent.edu.et>
2025-01-06 19:59:53 +0300

    Update Blackjack.cpp

--- Blackjack_Game_project/Blackjack.cpp
+++ Blackjack_Game_project/Blackjack.cpp
@@ -182,7 +182,8 @@ else {
                 player2Sum += 11;
                 player2AceCount++;
             }
-else if (player2Cards[player2CardCount] == 't' || player2Cards[player2CardCount] == 'j'|| player2Cards[player2CardCount] == 'q' || player2Cards[player2CardCount] == 'k') {
+                
+else if (player2Cards[player2CardCount] == 't' ||  player2Cards[player2CardCount] == 'j' || player2Cards[player2CardCount] == 'q' || player2Cards[player2CardCount] == 'k') {
                 player2Sum += 10;
             } else {
                 player2Sum += (player2Cards[player2CardCount] - '0');
@@ -221,6 +222,9 @@ else if (player2Cards[player2CardCount] == 't' || player2Cards[player2CardCount]
 
         totalRounds++; // Increment total rounds
     }
+
+    return 0;
+}
 
     return 0;
 }
